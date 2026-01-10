021ad5b41b68274f5fcb3ce0ace227aa665d8497
Quentin <faber.quentin@gmail.com>
2026-01-10 03:49:49 +0100

    pirate pictoonlineV3

--- photographie/src/services/tariffServiceV2.ts
+++ photographie/src/services/tariffServiceV2.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { TariffConfigV2, TariffCategoryV2 } from "../types/tarifConfigV2";
 
-const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5001";
+import { API_URL } from "../config/api";
 
 export const tariffServiceV2 = {
   getTariffConfig: async (): Promise<TariffConfigV2> => {
