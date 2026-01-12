4eb279e24d29f2e3a3355b70793ddc74a0078674
Quentin <faber.quentin@gmail.com>
2026-01-12 01:04:26 +0100

    correction push

--- photographie/src/pages/About.tsx
+++ photographie/src/pages/About.tsx
@@ -1,5 +1,5 @@
 import { useEffect } from "react";
-import { motion, useReducedMotion } from "framer-motion";
+import { motion } from "framer-motion";
 import Navbar from "../components/layout/navbar";
 import Footer from "../components/layout/Footer";
 import SEO from "../components/SEO";
