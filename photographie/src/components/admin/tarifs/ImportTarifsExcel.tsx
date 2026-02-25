21ead81d03c97fb695e30cd63f9c667af4264488
Quentin <faber.quentin@gmail.com>
2026-02-26 00:26:48 +0100

    add import fronte

--- photographie/src/components/admin/tarifs/ImportTarifsExcel.tsx
+++ photographie/src/components/admin/tarifs/ImportTarifsExcel.tsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
-
-const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5001";
+import { API_URL } from "../../../config/api";
 
 interface ImportStats {
   categories: number;
